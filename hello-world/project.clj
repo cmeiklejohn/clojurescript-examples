4a6693384ad24905e94b7683920a67da78f4090d
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-08-26 01:46:52 -0400

    Add resources and separate out files.

--- hello-world/project.clj
+++ hello-world/project.clj
@@ -2,4 +2,12 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.6"]]
-  :cljsbuild {:builds []})
+  :source-path "src/clj"
+  :cljsbuild {:builds [{
+    :source-path "src/cljs"
+    :compiler {
+      :output-to "resources/public/hello.js"
+      :optimizations :whitespace
+      :pretty-print true
+    }
+  }]})
