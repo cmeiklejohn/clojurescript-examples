b0241722f00df41028b4576339dbbb32bc6078a0
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-08-26 01:29:44 -0400

    Initial.

---
+++ hello-world/project.clj
@@ -0,0 +1,5 @@
+(defproject hello-world "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-cljsbuild "0.2.6"]]
+  :cljsbuild {:builds []})
